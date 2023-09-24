1bc4ba8c642f3806f597a71a9fa9bc10ef119580
Keven Huard-Dubé <kehua001@edu.cegepgarneau.ca>
2023-09-24 15:56:00 -0400

    Ajout d'un logo et d'un favicon et commencement du template de base

---
+++ projet_rando_web/Pages/Base.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace projet_rando_web.Pages
+{
+    public class BaseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
