c2b1c717324aeb525ad3f666ff729cfc7d7eb958
Keven Huard-Dubé <kehua001@edu.cegepgarneau.ca>
2023-10-04 21:17:33 -0400

    Affichage complet de page accueil et de la carte que j'ai passer vraiment trop de temps deçu

--- projet_rando_web/Program.cs
+++ projet_rando_web/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddBlazorBootstrap();
+builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddGeoBlazor();
 
 var app = builder.Build();
@@ -17,30 +17,22 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseExceptionHandler("/Error");
 }
 
-app.UseHttpsRedirection();
 
 var provider = new FileExtensionContentTypeProvider();
 provider.Mappings[".wsv"] = "application/octet-stream";
-
 
 app.UseStaticFiles();
 // NOTE: for some reason, you still need the plain "UseStaticFiles" call above
 app.UseStaticFiles(new StaticFileOptions
 {
     ContentTypeProvider = provider
-}); 
-
+});
 app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
-
-
-
