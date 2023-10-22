e8c798dd7d818842478c9ab87010f3da7a45dc58
Shynsa <61347233+Mirella972@users.noreply.github.com>
2023-10-22 16:05:30 -0400

    Affichage des randonnées avec la base de données.

--- projet_rando_web/Classes/Ville.cs
+++ projet_rando_web/Classes/Ville.cs
@@ -16,11 +16,6 @@ namespace projet_rando_web.Classes
         [BsonId, BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
-        /// <summary>
-        /// Obtient ou définit le pays auquel appartient la ville.
-        /// </summary>
-        [BsonIgnore]
-        public Pays Pays { get; set; }
 
         /// <summary>
         /// Obtient ou définit le nom de la ville.
@@ -29,16 +24,30 @@ namespace projet_rando_web.Classes
         public string Nom { get; set; }
 
         /// <summary>
+        /// Obtient ou définit le nom de la ville.
+        /// </summary>
+        [BsonElement("Latitude"), BsonRepresentation(BsonType.Decimal128)]
+        public float Latitude { get; set; }
+
+        /// <summary>
+        /// Obtient ou définit le nom de la ville.
+        /// </summary>
+        [BsonElement("Longitude"), BsonRepresentation(BsonType.Decimal128)]
+        public float Longitude { get; set; }
+
+        /// <summary>
         /// Initialise une nouvelle instance de la classe Ville avec un identifiant, un pays et un nom spécifiés.
         /// </summary>
         /// <param name="id">L'identifiant de la ville.</param>
-        /// <param name="pays">Le pays auquel appartient la ville.</param>
         /// <param name="nom">Le nom de la ville.</param>
-        public Ville(string id, Pays pays, string nom)
+        /// <param name="latitude">La latitude de la ville.</param>
+        /// <param name="longitude">La longitude de la ville.</param>
+        public Ville(string id, Pays pays, string nom, float latitude, float longitude)
         {
             Id = id;
-            Pays = pays;
             Nom = nom;
+            Latitude = latitude;
+            Longitude = longitude;
         }
     }
 }
