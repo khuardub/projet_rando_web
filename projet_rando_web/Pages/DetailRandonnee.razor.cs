18be542bd00ac3710b031ef8e09426e506a5ac43
Keven Huard-Dubé <kehua001@edu.cegepgarneau.ca>
2023-10-31 15:46:46 -0400

    Affichage d'un point sur la carte de détails

--- projet_rando_web/Pages/DetailRandonnee.razor.cs
+++ projet_rando_web/Pages/DetailRandonnee.razor.cs
@@ -3,7 +3,15 @@ using projet_rando_web.Classes;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using projet_rando_web.Data;
-
+using dymaptic.GeoBlazor.Core.Components.Layers;
+using dymaptic.GeoBlazor.Core.Components.Views;
+using System.Collections.Generic;
+using System.Threading;
+using System.Drawing;
+using projet_rando_web.Interfaces;
+using dymaptic.GeoBlazor.Core.Components.Geometries;
+using System.Dynamic;
+using dymaptic.GeoBlazor.Core.Components;
 
 namespace projet_rando_web.Pages
 {
@@ -14,12 +22,23 @@ namespace projet_rando_web.Pages
 
         [Parameter]
         public string Id { get; set; }
+        public GraphicsLayer graphicsLayer;
+        private MapView? mapView;
 
         protected override async Task OnInitializedAsync()
         {
             randonnee = randonneeService.GetRandonnee(Id);
         }
-
-        
+        private async Task OnViewRendered()
+        {
+            Geometry geo = new dymaptic.GeoBlazor.Core.Components.Geometries.Point(randonnee.EndroitDepart.Longitude, randonnee.EndroitDepart.Latitude);
+            
+            graphicsLayer = new GraphicsLayer();
+            Graphic graph1;
+            graph1 = new Graphic(geo);
+            graphicsLayer.Add(graph1);
+            mapView.AddLayer(graphicsLayer);
+        }
     }
+
 }
